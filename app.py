1e08aa7d3d85fedeb009d5152e5ee7695932a761
nixoninnes <nixoninnes@gmail.com>
2016-04-23 15:12:36 +0100

    Trying to bring some sane structure

--- app.py
+++ app.py
@@ -1,6 +1,67 @@
 import urwid
 
 
+class Main(object):
+    def __init__(self):
+        self.build_widgets()
+
+    palette = [
+        ('header', 'white', 'dark green', 'bold'),
+        ('tile', 'dark grey', 'white'),
+        ('edit', 'light grey', 'dark green')
+    ]
+
+    # Widgets
+    def create_header(self, header):
+        widget = urwid.Columns([
+            urwid.AttrWrap(urwid.Text(header), 'header'),
+            urwid.Button('x', on_press=self.quit_on_clicked)
+        ])
+        return widget
+
+    def create_edit(self, label, text, on_change):
+        widget = urwid.Edit(label, text, multiline=False)
+        urwid.connect_signal(widget, 'change', on_change)
+        on_change(widget, text)
+        widget = urwid.AttrWrap(widget, 'edit')
+        return widget
+
+    def create_tile(self):
+        widget = urwid.LineBox(urwid.Filler(urwid.Text('', align='left')))
+        widget = urwid.AttrWrap(widget, 'tile')
+        return widget
+
+    # Events
+    def edit_change_event(self, widget, text):
+        pass
+
+    def quit_on_clicked(self, button):
+        raise urwid.ExitMainLoop()
+
+    def build_widgets(self):
+        self.header = self.create_header('My Urwid Application')
+        self.edit = self.create_edit('>', '', self.edit_change_event)
+        self.tile1 = self.create_tile()
+        self.tile2 = self.create_tile()
+
+    # View
+    def build_view(self):
+        view = urwid.Frame(
+            urwid.Columns([
+                self.tile1,
+                self.tile2
+            ]),
+            header=self.header,
+            footer=self.edit
+        )
+        return view
+
+    def run(self):
+        urwid.MainLoop(
+            self.build_view()
+        ).run()
+
+
 class Input(urwid.Edit):
     def __init__(self):
         super().__init__('> ')
@@ -9,49 +70,9 @@ class Input(urwid.Edit):
     def keypress(self, size, key):
         if key != 'enter':
             return super().keypress(size, key)
-        
         self.set_edit_text('')
 
 
-def main():
-
-    title = urwid.Text('My Title')
-    quit_button = urwid.Button('X')
-
-    top_r = urwid.LineBox(urwid.Filler(urwid.Text('top r')))
-    top_m = urwid.LineBox(urwid.Filler(urwid.Text('top m')))
-    top_l = urwid.LineBox(urwid.Filler(urwid.Text('top l')))
-    bot_r = urwid.LineBox(urwid.Filler(urwid.Text('bot r')))
-    bot_m = urwid.LineBox(urwid.Filler(urwid.Text('bot m')))
-    bot_l = urwid.LineBox(urwid.Filler(urwid.Text('bot l')))
-
-    edit = Input()
-
-    layout = urwid.Frame(
-        urwid.Pile([
-            urwid.Columns([top_l,top_m,top_r]),
-            urwid.Columns([bot_l,bot_m,bot_r]),
-        ]),
-        header=urwid.Columns([title, quit_button]),
-        footer=edit
-    )
-
-    def quit_on_clicked(button):
-        raise urwid.ExitMainLoop()
-
-    def unhandled_input(key):
-        if key in ['q', 'Q']:
-            raise urwid.ExitMainLoop()
-
-    urwid.connect_signal(quit_button, 'click', quit_on_clicked)
-
-    loop = urwid.MainLoop(
-        layout,
-        unhandled_input=unhandled_input
-    )
-
-    loop.run()
-
-
 if __name__ == '__main__':
-    main()
+    app = Main()
+    app.run()
